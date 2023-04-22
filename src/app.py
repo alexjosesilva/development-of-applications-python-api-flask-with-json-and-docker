2819549876c52724ed36f36ab64d2846948576af
Alex Jose Silva <turmawb@gmail.com>
2023-04-22 13:43:59 -0300

    DEV | list all e save item function

--- src/app.py
+++ src/app.py
@@ -1,11 +1,36 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, json, request
+from flask_restful import Api
 
-@app.route('/listausuario')
-def get():
-        return {
-            'id': 123,
-            'name':"teste",
-            'age':40,
-            'country':'Brazil'
+app = Flask(__name__)
+api = Api(app)
+
+dados = [
+        {
+            "id":1,
+            "name_city": "Dublin",
+            "country": "Ireland"
+        },
+        {
+            "id":2,
+            "name_city": "Toronto",
+            "country": "Canada"
         }
+    ]
+
+@app.route("/listarTodos",  methods = ['GET'])
+def listarTodos():
+     return jsonify(dados)
+ 
+@app.route("/salvar",  methods = ['POST'])
+def salvar():  
+    dados.append(request.get_json())
+    return jsonify(dados)
+
+@app.route("/atualizarItem",  methods = ['PUT'])
+def atualizar():
+     return jsonify(dados)
+
+@app.route("/deletar",  methods = ['DELETE'])
+def deletar():
+     return jsonify(dados)
+ 
