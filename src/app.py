c484942c6f94cd9baaafb00731e33d562d4b62ac
Alex Jose Silva <turmawb@gmail.com>
2023-04-23 12:47:06 -0300

    DEV | development of applications python api flask with json and docker. Create, Read, Update, Delete, Search

--- src/app.py
+++ src/app.py
@@ -1,36 +1,57 @@
-from flask import Flask, jsonify, json, request
+from flask import Flask, jsonify, request, json
 from flask_restful import Api
+from json import dumps
 
 app = Flask(__name__)
 api = Api(app)
 
+#dados iniciais
 dados = [
-        {
-            "id":1,
-            "name_city": "Dublin",
-            "country": "Ireland"
-        },
-        {
-            "id":2,
-            "name_city": "Toronto",
-            "country": "Canada"
-        }
+            {
+                "id":1,
+                "name_city": "Dublin",
+                "country": "Ireland"
+            }
     ]
 
-@app.route("/listarTodos",  methods = ['GET'])
+# Listar todos registros
+@app.route("/listar",  methods = ['GET'])
 def listarTodos():
      return jsonify(dados)
- 
+
+# salvar um registro
 @app.route("/salvar",  methods = ['POST'])
 def salvar():  
     dados.append(request.get_json())
     return jsonify(dados)
 
-@app.route("/atualizarItem",  methods = ['PUT'])
-def atualizar():
-     return jsonify(dados)
+#atualizar um registro
+@app.route("/atualizarItem/<id>",  methods = ['PUT'])
+def atualizar(id):
+     
+    for key in dados:
+       if(dumps(key['id']) == id):
+            dados[dados.index(key)] = request.get_json()
+            return dados
+                   
+    return []
+    
+    
+#deletar um registro
+@app.route("/deletar/<id>",  methods = ['DELETE'])
+def deletar(id):
+    for key in dados:
+        if(dumps(key['id']) == id):
+            dados.remove(key)
+            return dados
+                   
+    return []
 
-@app.route("/deletar",  methods = ['DELETE'])
-def deletar():
-     return jsonify(dados)
- 
+#procurar um registro
+@app.route("/procurar/<name_city>",  methods = ['GET'])
+def procurar(name_city):
+    for key in dados:
+        if key['name_city'] == name_city:
+            return jsonify(dados[dados.index(key)])
+                   
+    return []
